456959403d3f6ee948bd7a5f988895807fb73d78
Marius BRAETS <mbraets@student.42.fr>
2022-06-08 18:28:37 +0200

    ex02 - finish waiting selfeval

---
+++ ex02/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << std::endl << "/********* Memory Address *********\\" << std::endl;
+
+	std::cout << "string:		" << &string << std::endl;
+	std::cout << "stringPTR:	" << stringPTR << std::endl;
+	std::cout << "stringREF:	" << &stringREF << std::endl;
+
+	std::cout << std::endl << "/************* Value **************\\" << std::endl;
+	std::cout << "string:		" << string << std::endl;
+	std::cout << "stringPTR:	" << *stringPTR << std::endl;
+	std::cout << "stringREF:	" << stringREF << std::endl;
+}
