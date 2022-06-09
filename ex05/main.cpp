9fb895dd8ee64289799af3a31634e7478c4e34a7
Marius BRAETS <mbraets@student.42.fr>
2022-06-09 13:03:13 +0200

    ex05 - finish

---
+++ ex05/main.cpp
@@ -0,0 +1,25 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+
+	Harl	harl;
+
+
+	std::cout << std::endl << "[ DEBUG ]" << std::endl;
+	harl.complain("DEBUG");
+
+	std::cout << std::endl << "[ INFO ]" << std::endl;
+	harl.complain("INFO");
+
+	std::cout << std::endl << "[ WARNING ]" << std::endl;
+	harl.complain("WARNING");
+
+	std::cout << std::endl << "[ ERROR ] " << std::endl;
+	harl.complain("ERROR");
+
+	std::cout << std::endl << "[ undefined ]" << std::endl;
+	harl.complain("undefined");
+	
+	return (0);
+}
